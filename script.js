96738129d7bdf65c1db06c2a23a31b01e90108d8
unknown <maheenfiyyaz>
2024-08-16 15:55:57 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+// 01
+var mcqs_1 = prompt("What will the following code output and why?");
+if(mcqs_1 === "22" ){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 02
+var mcqs_2 = prompt("How can you create a text gradient effect using only CSS?");
+if(mcqs_2 === "if (i == 5) { }"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 03
+var mcqs_3 = prompt("Where is the correct place to insert a JavaScript?");
+if(mcqs_3 === "The body section"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 04
+var mcqs_4 = prompt("What does the === operator do in JavaScript?.");
+if(mcqs_4 === "Compares both value and type"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 05
+var mcqs_5 = prompt("Which of the following is a way to create an object in JavaScript?");
+if(mcqs_5 === "All of the above"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 06
+var mcqs_6 = prompt("What is the correct HTML element for inserting a line break?");
+if(mcqs_6 === "br"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 07
+var mcqs_7 = prompt("Choose the correct HTML element to define important text");
+if(mcqs_7 === "strong"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 08
+var mcqs_8 = prompt("Choose the correct HTML element to define emphasized text");
+if(mcqs_8 === "em"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 09
+var mcqs_9 = prompt("What will the following code return?");
+if(mcqs_9 === "number"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 10
+var mcqs_10 = prompt("What will the following code log?");
+if(mcqs_10 === "42"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
